466bb853271c8a40b8ac5d296daf743df1ee6d62
Aaron Gertler <aaron.gertler@gmail.com>
2020-10-02 14:58:46 -0500

    applying Animal functionality to other sections

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,9 +2,12 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import { Home } from './Home'
 import { LocationCard } from './location/LocationCard'
-import { AnimalCard } from './animal/AnimalCard'
-import { CustomerCard } from './customer/CustomerCard'
-import { EmployeeCard } from './employee/EmployeeCard'
+import { AnimalProvider } from './animal/AnimalProvider'
+import { AnimalList } from './animal/AnimalList'
+import { CustomerProvider } from './customer/CustomerProvider'
+import { CustomerList } from './customer/CustomerList'
+import { EmployeeProvider } from './employee/EmployeeProvider'
+import { EmployeeList } from './employee/EmployeeList'
 
 export const ApplicationViews = (props) => {
     return (
@@ -20,19 +23,25 @@ export const ApplicationViews = (props) => {
             </Route>
 
             {/* Render the animal list when http://localhost:3000/animals */}
-            <Route path='/animals'>
-                <AnimalCard />
-            </Route>
+            <AnimalProvider>
+                <Route exact path='/animals'>
+                    <AnimalList />
+                </Route>
+            </AnimalProvider>
 
             {/* Render the customer list when http://localhost:3000/customers */}
-            <Route path='/customers'>
-                <CustomerCard />
-            </Route>
+            <CustomerProvider>
+                <Route exact path='/customers'>
+                    <CustomerList />
+                </Route>
+            </CustomerProvider>
 
             {/* Render the employee list when http://localhost:3000/employees */}
-            <Route path='/employees'>
-                <EmployeeCard />
-            </Route>
+            <EmployeeProvider>
+                <Route exact path='/employees'>
+                    <EmployeeList />
+                </Route>
+            </EmployeeProvider>
         </>
     )
 }
